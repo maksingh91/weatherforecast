8a84a6913e1834668eb182a0d5b3ae41a49388fc
Mahaveer Ratnoo <mahaveerratnoo@192.168.1.8>
2023-01-30 22:17:34 +0530

    initial logic

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # weatherforecast
 Weather Forecast application with CICD using Docker
+
+
+Docker Image can be pulled from Docker hub using following command
+
+docker pull mahaveer91/weatherforecast-cicd-image
+
+Run Docker Container using 
+
+docker run -p 8080:8080 mahaveer91/weatherforecast-cicd-image
+
+Access the API Swagger documentations at http://localhost:8080/swagger-ui/index.html#/
--- src/main/java/com/sample/project/weatherforecast/controller/ForecastController.java
+++ src/main/java/com/sample/project/weatherforecast/controller/ForecastController.java
@@ -1,11 +1,20 @@
 package com.sample.project.weatherforecast.controller;
 
 import com.sample.project.weatherforecast.model.ForecastResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,8 +23,29 @@ import java.util.List;
 @RestController
 public class ForecastController {
 
-    @GetMapping
-    public ResponseEntity<List<ForecastResponse>> forecastTheWeather(){
+    @GetMapping(value = "/forecast/{city}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Lists three days weather forecast.",
+            description = "Lists Upcoming three days weather forecast with predictions",
+            responses = {
+                    @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+                    @ApiResponse(responseCode = "500",description = "Internal Server Error", content = @Content),
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value =  "[{\"date\" : \"17 Feb 2023\", \"highTemp\" : 45, \"lowTemp\" :23, \"predictions\":[\"Carry Umbrella\",\"Use Sunscreen Lotion\"]}]"
+                                            ),
+                                    },
+                                    array = @ArraySchema( schema = @Schema(implementation =  ForecastResponse.class))
+
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<List<ForecastResponse>> forecastTheWeather(@PathVariable("city") String city){
 
         List<ForecastResponse> lst = new ArrayList<ForecastResponse>();
         lst.add(new ForecastResponse());
